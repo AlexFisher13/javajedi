a8347bc082929d8648f545e78b6c086d84b11483
Fisher <alexfisher13@yandex.ru>
2021-07-03 22:21:15 +0300

    move menu to js

---
+++ js/menu.js
@@ -0,0 +1,73 @@
+document.write("<nav class='pushy pushy-left' data-focus='#first-link'>\n" +
+    "    <div class='pushy-content'>\n" +
+    "        <ul>\n" +
+    "            <li class='pushy-submenu'>\n" +
+    "                <button>Java</button>\n" +
+    "                <ul>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/java.html'>Java</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/collections.html'>Коллекции</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/multithreading.html'>Многопоточность</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/java8.html'>Java 8</a></li>\n" +
+    "                </ul>\n" +
+    "            </li>\n" +
+    "            <li class='pushy-submenu'>\n" +
+    "                <button>Web</button>\n" +
+    "                <ul>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/internet.html'>Internet</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/http.html'>HTTP</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/webservices.html'>SOAP WebServices and REST</a></li>\n" +
+    "                </ul>\n" +
+    "            </li>\n" +
+    "            <li class='pushy-submenu'>\n" +
+    "                <button>Базы данных</button>\n" +
+    "                <ul>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/database.html'>Типы баз данных</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/transaction.html'>Транзакции</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/jdbc.html'>Hibernate JPA JDBC</a></li>\n" +
+    "                </ul>\n" +
+    "            </li>\n" +
+    "            <li class='pushy-submenu'>\n" +
+    "                <button>Автоматизация</button>\n" +
+    "                <ul>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/tddbdd.html'>BDD/TDD jUnit/TestNG</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/selenium.html'>Selenuim</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/selectors.html'>Локаторы</a></li>\n" +
+    "                </ul>\n" +
+    "            </li>\n" +
+    "            <li class='pushy-submenu'>\n" +
+    "                <button>Паттерны</button>\n" +
+    "                <ul>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/singleton.html'>Singleton</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/factorymethod.html'>Factory method</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/prototype.html'>Prototype</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/iterator.html'>Iterator</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/adapter.html'>Adapter</a></li>\n" +
+    "                </ul>\n" +
+    "            </li>\n" +
+    "            <li class='pushy-submenu'>\n" +
+    "                <button>Spring</button>\n" +
+    "                <ul>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/spring.html'>Spring</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/annotation.html'>Аннотации</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/springboot.html'>Spring Boot</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/beans.html'>Жизненный цикл бина</a></li>\n" +
+    "                </ul>\n" +
+    "            </li>\n" +
+    "            <li class='pushy-submenu'>\n" +
+    "                <button>Docker, DevOps</button>\n" +
+    "                <ul>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/docker.html'>Docker</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/cicd.html'>CI/CD</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/containerization.html'>Контейнеризация</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/devopsother.html'>Elastic, Kibana, Heroku</a></li>\n" +
+    "                </ul>\n" +
+    "            </li>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/servlets.html'>Servlets, WebServers</a></li>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/maven.html'>Maven</a></li>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/scrum.html'>Scrum</a></li>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/git.html'>Git</a></li>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/regex.html'>Регулярка</a></li>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/app.html'>App (Heroku,Mongo,React Native,Spring Boot)</a></li>\n" +
+    "        </ul>\n" +
+    "    </div>\n" +
+    "</nav>")
