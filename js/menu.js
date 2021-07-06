5ca16b6851af02264ff8a268f101a68b11910aec
alexfisher13 <alexfisher13@yandex.ru>
2021-07-06 16:01:12 +0300

    add mockito

--- js/menu.js
+++ js/menu.js
@@ -15,11 +15,12 @@ document.write("<nav class='pushy pushy-left' data-focus='#first-link'>\n" +
     "                <ul>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/internet.html'>Internet</a></li>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/http.html'>HTTP</a></li>\n" +
-    "                    <li class='pushy-link'><a href='/javajedi/pages/webservices.html'>SOAP WebServices and REST</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/servlets.html'>Servlets, WebServers</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/webservices.html'>SOAP and REST</a></li>\n" +
     "                </ul>\n" +
     "            </li>\n" +
     "            <li class='pushy-submenu'>\n" +
-    "                <button>Базы данных</button>\n" +
+    "                <button>Data bases</button>\n" +
     "                <ul>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/database.html'>Типы баз данных</a></li>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/transaction.html'>Транзакции</a></li>\n" +
@@ -27,15 +28,16 @@ document.write("<nav class='pushy pushy-left' data-focus='#first-link'>\n" +
     "                </ul>\n" +
     "            </li>\n" +
     "            <li class='pushy-submenu'>\n" +
-    "                <button>Автоматизация</button>\n" +
+    "                <button>Testing</button>\n" +
     "                <ul>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/tddbdd.html'>BDD/TDD jUnit/TestNG</a></li>\n" +
+    "                    <li class='pushy-link'><a href='/javajedi/pages/mockito.html'>Mockito</a></li>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/selenium.html'>Selenuim</a></li>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/selectors.html'>Локаторы</a></li>\n" +
     "                </ul>\n" +
     "            </li>\n" +
     "            <li class='pushy-submenu'>\n" +
-    "                <button>Паттерны</button>\n" +
+    "                <button>Patterns</button>\n" +
     "                <ul>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/singleton.html'>Singleton</a></li>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/factorymethod.html'>Factory method</a></li>\n" +
@@ -54,7 +56,7 @@ document.write("<nav class='pushy pushy-left' data-focus='#first-link'>\n" +
     "                </ul>\n" +
     "            </li>\n" +
     "            <li class='pushy-submenu'>\n" +
-    "                <button>Docker, DevOps</button>\n" +
+    "                <button>DevOps</button>\n" +
     "                <ul>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/docker.html'>Docker</a></li>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/cicd.html'>CI/CD</a></li>\n" +
@@ -62,11 +64,10 @@ document.write("<nav class='pushy pushy-left' data-focus='#first-link'>\n" +
     "                    <li class='pushy-link'><a href='/javajedi/pages/devopsother.html'>Elastic, Kibana, Heroku</a></li>\n" +
     "                </ul>\n" +
     "            </li>\n" +
-    "            <li class='pushy-link'><a href='/javajedi/pages/servlets.html'>Servlets, WebServers</a></li>\n" +
     "            <li class='pushy-link'><a href='/javajedi/pages/maven.html'>Maven</a></li>\n" +
     "            <li class='pushy-link'><a href='/javajedi/pages/scrum.html'>Scrum</a></li>\n" +
     "            <li class='pushy-link'><a href='/javajedi/pages/git.html'>Git</a></li>\n" +
-    "            <li class='pushy-link'><a href='/javajedi/pages/regex.html'>Регулярка</a></li>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/regex.html'>Regex</a></li>\n" +
     "            <li class='pushy-link'><a href='/javajedi/pages/app.html'>App (Heroku,Mongo,React Native,Spring Boot)</a></li>\n" +
     "        </ul>\n" +
     "    </div>\n" +
